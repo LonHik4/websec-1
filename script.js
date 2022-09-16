a945feb31cd6f590833a2cea9dc7f09f9068fcae
LonHik4 <92443023+LonHik4@users.noreply.github.com>
2022-09-16 10:03:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
+
 function buttonClickHandler(){
     var value1 = document.getElementById("number1").value;
     var value2 = document.getElementById("number2").value;
-    if (!isNaN(parseFloat(value1)) && isFinite(value1) && !isNaN(parseFloat(value2)) && isFinite(value2))
-    {
-        if (document.getElementById("select1").value == "add"){
-            let result = parseFloat(value1) + parseFloat(value2);
-            document.getElementById("results").innerText= result;
+    var select = document.getElementById("select1").value;
+    var result=0.0;
+        if (select == "add" ){
+            result = parseFloat(value1) + parseFloat(value2);
+            //document.getElementById("results").innerText= result;
         }
-    
-        if (document.getElementById("select1").value == "sub"){
-            let result = parseFloat(value1) - parseFloat(value2);
-            document.getElementById("results").innerText= result;
+        if (select == "sub"){
+            result = parseFloat(value1) - parseFloat(value2);
         }
-        
-        if (document.getElementById("select1").value == "mult"){
-            let result = parseFloat(value1) * parseFloat(value2);
-            document.getElementById("results").innerText= result;
+        if (select == "mult"){
+            result = parseFloat(value1) * parseFloat(value2);
         }
-    
-        
-        if (document.getElementById("select1").value == "div"){
-            let result = parseFloat(value1) / parseFloat(value2);
-            document.getElementById("results").innerText= result;
+        if (select == "div"){
+            result = parseFloat(value1) / parseFloat(value2);
         }
-    }
-    else{
-        let result = "you didn't enter a number";
         document.getElementById("results").innerText= result;
-    }
-    
 }
