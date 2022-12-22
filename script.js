1b1711dd6384de3f5f63091aaf0f1bb862c0f1f2
LonHik4 <92443023+LonHik4@users.noreply.github.com>
2022-12-22 18:52:19 +0400

    Update code

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function buttonClickHandler(){
     var value1 = document.getElementById("number1").value;
     var value2 = document.getElementById("number2").value;
     var select = document.getElementById("select1").value;
-    var result=0.0;
+	if (value1==="" || value2===""){
+        alert('Внимание!\nВведите оба числа!');
+        return;
+    }else{
+		var result=0.0;
         if (select == "add" ){
             result = parseFloat(value1) + parseFloat(value2);
             //document.getElementById("results").innerText= result;
@@ -15,7 +19,12 @@ function buttonClickHandler(){
             result = parseFloat(value1) * parseFloat(value2);
         }
         if (select == "div"){
-            result = parseFloat(value1) / parseFloat(value2);
+			if (value2 === "0") {
+                alert("Внимание!\nНа ноль делить нельзя")
+            }else{
+				result = parseFloat(value1) / parseFloat(value2);
+			}
         }
         document.getElementById("results").innerText= result;
+	}
 }
